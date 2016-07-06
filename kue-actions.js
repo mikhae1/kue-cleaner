09e4c70df2f0cb2f60183cab7e5597946e644824
mmekhanov <mikhail.mekhanov@eurochem.ru>
2016-07-06 16:33:15 +0300

    Add initial checks

--- kue-actions.js
+++ kue-actions.js
@@ -2,21 +2,37 @@
 
 var kue = require('kue');
 var Redis = require('ioredis');
+var chalk = require('chalk');
 
 var lib = require('./lib');
 var config = require('./config');
+var path = require('path');
+var fs = require('fs');
 
 var yargs = require('yargs')
-  .usage('Usage: $0 <target> <task> [options]')
-  .demand(2);
+  .usage('Usage: $0 <target> <task> [options]');
+  //.demand(2);
 
 var argv = yargs.argv;
-
 var targets = config.targets;
-var target = targets[argv._.shift()];
+var target = argv._.shift();
 var task = argv._.shift();
 
-var queue = create(target);
+if (!target) return error('Define target');
+if (!task) return error('Specify task');
+
+if (!targets.hasOwnProperty(target)) return error('Unknown target');
+
+var taskPath = path.join('tasks', task + '.js');
+
+try {
+  fs.accessSync(taskPath);
+} catch (e) {
+  error('Task "%s" not found at %s', task, taskPath);
+}
+
+var config = targets[target];
+var queue = create(config);
 
 queue.on('error', function(err) {
   lib.error(err);
@@ -25,7 +41,7 @@ queue.on('error', function(err) {
 
 lib.init(queue);
 
-require('./tasks/' + task + '.js')(queue, yargs);
+require('./' + taskPath)(queue, yargs);
 
 // always use ioredis! (nd-queue default)
 function create(config) {
@@ -38,4 +54,10 @@ function create(config) {
       }
     }
   });
+}
+
+function error(err) {
+  console.error(chalk.red(err));
+
+  process.exit(1);
 }
